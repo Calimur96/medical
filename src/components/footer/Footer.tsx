0030b8b50d523bf7f5b2d71c30da668876f4cee5
EaleksS <ealgenv@gmail.com>
2023-05-09 03:22:47 +0900

    анимация для call

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,18 +1,19 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "./Footer.scss";
-import {
-  BsChatSquareText,
-  BsHouseHeart,
-  BsSearch,
-  BsTelephoneX,
-} from "react-icons/bs";
+import { BsChatSquareText, BsHouseHeart, BsSearch } from "react-icons/bs";
 
 const Footer: FC = (): JSX.Element => {
+  const [isActive, setiIsActive] = useState<boolean>(Boolean);
+
   return (
     <footer className="footer">
       <div className="footer__container">
-        <div className="footer__call">
-          <BsTelephoneX />
+        <div
+          className={`footer__call ${isActive && "active"}`}
+          onClick={() => setiIsActive((prev) => !prev)}
+        >
+          <img src="/phone.png" alt="phone" className="footer__call-phone" />
+          <img src="/plus.png" alt="plus" className="footer__call-plus" />
         </div>
       </div>
       <ul className="footer__ul">
