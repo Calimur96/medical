825e7cf323c6700b41a293d4b18affa66bdcc824
Evgeny Trizno <zenatrizno9@gmail.com>
2023-05-09 16:07:12 +0300

    fix

--- src/components/accountInfo/AccountInfo.tsx
+++ src/components/accountInfo/AccountInfo.tsx
@@ -1,12 +1,42 @@
 import { FC } from "react";
+import { AiOutlineDownload } from "react-icons/ai";
 
+import avatar from "../../assets/avatar.png";
 import "./AccountInfo.scss";
 
 const AccountInfo: FC = () => {
     return (
         <div className="account-info">
             <div className="account-info__inner">
-                <div className="account-info__profile"></div>
+                <div className="account-info__profile">
+                    <div className="account-info__img-wrapper">
+                        <img
+                            src={avatar}
+                            alt=""
+                            className="account-info__img"
+                        />
+                        <div className="input__wrapper">
+                            <input
+                                name="file"
+                                type="file"
+                                id="input__file"
+                                className="input__file"
+                                multiple
+                            />
+                            <label
+                                htmlFor="input__file"
+                                className="input__file-button"
+                            >
+                                <span className="input__file-icon-wrapper">
+                                    <AiOutlineDownload size={25} />
+                                </span>
+                                <span className="input__file-button-text">
+                                    Выберите файл
+                                </span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
