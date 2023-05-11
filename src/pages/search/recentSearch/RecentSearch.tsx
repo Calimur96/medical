a104ecdc0d3a865bb4af3c062fb4f8b78b461ffe
EaleksS <ealgenv@gmail.com>
2023-05-11 19:36:46 +0900

    .

--- src/pages/search/recentSearch/RecentSearch.tsx
+++ src/pages/search/recentSearch/RecentSearch.tsx
@@ -8,7 +8,7 @@ const RecentSearch: FC = () => {
     <div className="recent-search">
       <div className="recent-search__inner">
         {[1, 2, 3, 4, 5].map((item) => (
-          <div className="search-page__item">
+          <div className="search-page__item" key={item}>
             <div className="search-page__item-content">
               <img src={avatar} alt="" className="search-page__item-img" />
             </div>
